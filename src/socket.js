0419cf73daf29a017e027d1d6ad20b2ee5f01985
Frosty <y.faustina@op.iitg.ac.in>
2026-02-01 23:08:28 +0530

    fix: fallback socket URL for Netlify env issue

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,11 @@
 import { io } from "socket.io-client";
 
-export const socket = io(import.meta.env.VITE_SOCKET_URL, {
+const SOCKET_URL =
+  import.meta.env.VITE_SOCKET_URL ||
+  "https://mini-docs-twm4.onrender.com";
+
+console.log("SOCKET URL:", SOCKET_URL);
+
+export const socket = io(SOCKET_URL, {
   transports: ["websocket"],
 });
